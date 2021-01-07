004b4017555b9073ebe5cda9b1ad804ffe8e398b
Ming Cheng <cmgrace@bu.edu>
2021-01-07 15:00:24 -0500

    prepare for deploy

--- server.js
+++ server.js
@@ -1,15 +1,89 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const POKEDEX = require("./pokedex.json");
+const cors = require("cors");
+const helmet = require("helmet");
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(morgan("dev"));
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
+app.use(morganSetting);
+app.use(helmet());
+app.use(cors());
+app.use(function validateBearerToken(req, res, next) {
+  const authToken = req.get("Authorization");
+  const apiToken = process.env.API_TOKEN;
 
-app.use((req, res) => {
-  res.send("Hello, world!");
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+
+  console.log("validate bearer token middleware");
+
+  next();
 });
 
-const PORT = 8000;
+const validTypes = [
+  `Bug`,
+  `Dark`,
+  `Dragon`,
+  `Electric`,
+  `Fairy`,
+  `Fighting`,
+  `Fire`,
+  `Flying`,
+  `Ghost`,
+  `Grass`,
+  `Ground`,
+  `Ice`,
+  `Normal`,
+  `Poison`,
+  `Psychic`,
+  `Rock`,
+  `Steel`,
+  `Water`,
+];
+
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
+
+app.get("/types", handleGetTypes);
+
+function handleGetPokemon(req, res) {
+  let response = POKEDEX.pokemon;
+
+  // filter our pokemon by name if name query param is present
+  if (req.query.name) {
+    response = response.filter((pokemon) =>
+      // case insensitive searching
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    );
+  }
+
+  // filter our pokemon by type if type query param is present
+  if (req.query.type) {
+    response = response.filter((pokemon) =>
+      pokemon.type.includes(req.query.type)
+    );
+  }
+
+  res.json(response);
+}
+
+app.get("/pokemon", handleGetPokemon);
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
